39e386ab380d331845f51c7b34698b53b011b9f9
Shrimaj <shrimaj.mehta60@gmail.com>
2021-12-14 16:18:08 +0530

    Add project files.

---
+++ Product_Review_Mgt_LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Product_Review_Mgt_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management Program using LINQ");
+        }
+    }
+}
